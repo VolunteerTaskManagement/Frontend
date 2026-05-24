5cb964621f5781c9a1345c9d90257d596c59a7d4
Amir Mohammad Azizi <amirazizigh@gmail.com>
2026-05-24 20:19:23 +0330

    fix: main routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import Login from './pages/Login/Login'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ChakraProvider value={defaultSystem}>
       <BrowserRouter>
-        <App />
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
       </BrowserRouter>
     </ChakraProvider>
   </StrictMode>,
