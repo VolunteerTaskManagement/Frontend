6abfaafb03e4201545906b4e80eebcc95803fb49
Amir Mohammad Azizi <amirazizigh@gmail.com>
2026-05-25 02:06:44 +0330

    feat: add signup page

---
+++ src/pages/SignUp/Signup.tsx
@@ -0,0 +1,11 @@
+import MobileLayout from "../../layouts/MobileLayout";
+import SignUpCard from "../../components/SignUp/SignUpCard";
+const SignUp = () => {
+  return (
+    <MobileLayout>
+        <SignUpCard />
+    </MobileLayout>
+  );
+};
+
+export default SignUp;
