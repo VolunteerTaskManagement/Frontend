b6a4b4d4eddeade63352fcfead0629fa3693e817
Amir Mohammad Azizi <amirazizigh@gmail.com>
2026-06-13 01:09:45 +0330

    fix: button not editable

--- src/components/common/MainButton.tsx
+++ src/components/common/MainButton.tsx
@@ -8,7 +8,6 @@ interface MainButtonProps {
 const MainButton = ({ text, ...props }: MainButtonProps) => {
   return (
     <Button
-    {...props}
       w="30%"
       h="40px"
       bg="#008A8F" 
@@ -16,8 +15,8 @@ const MainButton = ({ text, ...props }: MainButtonProps) => {
       borderRadius="10px"
       fontWeight="bold"
       fontSize="lg"
-    //   boxShadow="0 4px 0px rgba(0, 0, 0, .08)"
-    boxShadow="0 4px 6px rgba(0, 138, 143, 0.3)" 
+      boxShadow="0 4px 6px rgba(0, 138, 143, 0.3)" 
+      {...props}
     >
       {text}
     </Button>
