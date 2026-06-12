3f2cba70d3d2b0c86cb54c40556f6e1f04d4dbb4
Amir Mohammad Azizi <amirazizigh@gmail.com>
2026-06-13 01:11:53 +0330

    feat: add volunteer profile page

---
+++ src/pages/VolunteerProfile/VolunteerProfile.tsx
@@ -0,0 +1,14 @@
+import MobileLayout from "../../layouts/MobileLayout";
+import VolunteerProfileCard from "../../components/VolunteerProfile/VolunteerProfileCard";
+
+const VolunteerProfile = () => {
+  return (
+    <>
+    <MobileLayout>        
+      <VolunteerProfileCard />
+    </MobileLayout>
+    </>
+  );
+};
+
+export default VolunteerProfile;
