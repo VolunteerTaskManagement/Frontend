40ecb6f920d5b02189e23873204279a21ab7b796
Amir Mohammad Azizi <amirazizigh@gmail.com>
2026-06-29 23:55:00 +0330

    feat: add AuthProvider to main tags

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,12 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
 import Login from './pages/Login/Login'
 import SignUp from './pages/SignUp/Signup'
+import { AuthProvider } from './contexts/AuthContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ChakraProvider value={defaultSystem}>
+      <AuthProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Navigate to="/login" replace />} />
@@ -16,6 +18,7 @@ createRoot(document.getElementById('root')!).render(
           <Route path="/signup" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
+      </AuthProvider>
     </ChakraProvider>
   </StrictMode>
 )
